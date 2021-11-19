796aa23debced9a28e3a7a00076e33feba7e5956
David Tran <davidtran641@gmail.com>
2021-11-19 21:32:21 +0800

    Initial Commit

---
+++ SwiftUIClassDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUIClassDemo
+//
+//  Created by david.tran on 19/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
